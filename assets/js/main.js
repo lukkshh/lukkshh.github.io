02a2f71273ef37ec1218c659c01a384ab5867adc
lukkshh <xlukkass@gmail.com>
2023-12-26 12:19:42 +0400

    removed home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,6 @@ $(document).ready(function () {
     loop: true,
   });
 
-  typed.stop();
-
   const geo =
     "მე ვარ დამწყები დეველოპერი საქართველოდან, თავისუფალ დროს ვწერ კოდს, პროგრაპირების სწავლა დავიწყე როდესაც ვიყავი 16ის, მიყვარს bugების პოვნა და მათი გამოსწორება, მესმის სამი ენა: ქართული (მშობლიური), ინგლისური (Upper Intermediate), და რუსული (საბაზისო)";
   const eng =
@@ -39,7 +37,6 @@ $(document).ready(function () {
 
     if (ischecked === false) {
       if (scrollposY > 350) {
-        typed.start();
         ischecked = true;
       }
     }
@@ -58,59 +55,3 @@ function scrollup() {
   window.scroll(0, 0);
   window.location.hash = "";
 }
-
-particlesJS("particles-js", {
-  particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
-    shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 },
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 6,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 },
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: true, mode: "grab" },
-      onclick: { enable: true, mode: "push" },
-      resize: true,
-    },
-    modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
-    },
-  },
-  retina_detect: true,
-});
