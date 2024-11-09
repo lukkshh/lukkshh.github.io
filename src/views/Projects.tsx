b548be2644ce22a9bcf1544a3f060d2546f324d5
lukkshh <xlukkass@gmail.com>
2024-11-09 16:52:31 +0400

    add projects card

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,41 +1,45 @@
-import { GitHubIcon, LinkIcon } from "../components/Icons";
-import Badge from "../components/Badge";
+import { ProjectData } from "../components/ProjectCard";
+import ProjectCard from "../components/ProjectCard";
+import { useEffect, useState } from "react";
 
 const Projects = () => {
+  const [projects, setProjects] = useState<Array<ProjectData>>([]);
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch("/projects-data.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setProjects(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Oops! Error fetching data");
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <section className="w-full flex flex-col justify-center items-center text-white max-lg:p-2">
-      <div className="grid grid-cols-3 gap-16 mt-12 mb-12 max-sm:grid-cols-1 max-lg:grid-cols-2 ">
-        <div className="w-[380px] h-[330px] max-sm:w-[350px] bg-zinc-900 rounded-xl">
-          <div className="flex justify-center items-center">
-            <img
-              className="h-[190px] w-[300px] p-2 rounded-3xl object-fit"
-              src="/images/ecommerce.png"
-              alt="E-Commerce"
-            />
-          </div>
-          <p className="text-lg ml-4 mb-2">E-Commerce Website</p>
-          <a
-            target="_blank"
-            href="https://github.com/lukkshh/scandiweb"
-            className="text-base ml-4 flex mb-[2px] items-center hover:underline transition-all text-zinc-400"
-          >
-            <GitHubIcon /> Github Repository
-          </a>
-          <a
-            target="_blank"
-            href="https://lukkshh-scandiweb.ct.ws/"
-            className="text-base ml-4 flex items-center hover:underline transition-all text-zinc-400"
-          >
-            <LinkIcon /> Live Website
-          </a>
-          <div className="w-full pl-2 pr-2 mt-2 space-y-2">
-            <Badge>React</Badge>
-            <Badge>PHP</Badge>
-            <Badge>MySQL</Badge>
-            <Badge>TailwindCSS</Badge>
-          </div>
+    <section className="w-full flex  justify-center items-center text-white max-lg:p-2">
+      {loading ? (
+        <div className="w-full h-[92vh] flex justify-center items-center">
+          <span className="loader"></span>
         </div>
-      </div>
+      ) : error ? (
+        <p className="text-xl mt-12">{error}</p>
+      ) : (
+        <div className="grid grid-cols-3 gap-16 mt-12 mb-12 max-2xl:gap-8 max-sm:gap-6 max-sm:mt-6 max-sm:grid-cols-1 max-lg:grid-cols-2 ">
+          {projects.map((project, index) => (
+            <ProjectCard key={index} data={project} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
