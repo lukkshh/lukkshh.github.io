90cc406092be12bcf444a89b63d8076920fb5d5e
lukkshh <xlukkass@gmail.com>
2025-10-10 05:22:10 +0400

    refactor header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,19 @@ import { useLanguage } from "../context/LanguageContext";
 
 import LanguageButton from "./LanguageButton";
 
+type NavNavLinkType = {
+  href: string;
+  label: string;
+}[];
+
 const Header = (): JSX.Element => {
   const { language, isEn } = useLanguage();
+
+  const NavLink: NavNavLinkType = [
+    { href: "#", label: language.Header.Home },
+    { href: "#about", label: language.Header.About },
+    { href: "#projects", label: language.Header.Projects },
+  ];
 
   const [scrollDirection, setScrollDirection] = useState("up");
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -36,24 +47,23 @@ const Header = (): JSX.Element => {
         scrollDirection === "down"
           ? "-translate-y-full opacity-0"
           : "translate-y-0 opacity-100"
-      }`}
+      }
+        ${isEn ? "font-en" : "font-ge"}
+      
+        `}
     >
       <header className="w-[398px] h-[55px] md:h-[60px] md:min-w-[450px] mx-[16px] my-[28px] px-[6px] py-[20px] rounded-xl flex justify-center items-center bg-gradient-to-br from-[#04071D] to-[#0C0E23] drop-shadow-sm border-[0.1px] border-[#36374942]">
         <nav>
           <ul
             className={` ${
               isEn ? "text-base" : "text-sm"
-            } flex justify-center items-center  md:text-lg font-medium space-x-6 text-white`}
+            } flex justify-center items-center text-white md:text-lg font-medium space-x-6`}
           >
-            <li>
-              <a href="">{language.Header.Home}</a>
-            </li>
-            <li>
-              <a href="">{language.Header.About}</a>
-            </li>
-            <li>
-              <a href="">{language.Header.Projects}</a>
-            </li>
+            {NavLink.map((link) => (
+              <li key={link.href}>
+                <a href={link.href}>{link.label}</a>
+              </li>
+            ))}
             <li>
               <LanguageButton />
             </li>
